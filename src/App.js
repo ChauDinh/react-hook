6c663ad5d3df4ab7ca21c2f920b1794f3dc1da57
Chau Dinh <chaudvb95uc@gmail.com>
2019-07-04 20:19:54 +0700

    useState() - we can use multiple useState hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className="App">Hello</div>;
+  const [count, setCount] = useState(10);
+  const [count2, setCount2] = useState(20);
+  return (
+    <div className="App">
+      <div>count1: {count}</div>
+      <div>count2: {count2}</div>
+      <button onClick={() => setCount(currentCount => currentCount + 1)}>
+        +
+      </button>
+    </div>
+  );
 };
 
 export default App;
