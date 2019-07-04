afba2e25febe3c6d78868deac5b1c65fa9a4fe3c
Chau Dinh <chaudvb95uc@gmail.com>
2019-07-04 21:21:46 +0700

    useEffect() - log render when the UI change, pass the value to call the function render when the value changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { useForm } from "./useForm";
 
 const App = () => {
-  const [values, handleChange] = useForm({ email: "", password: "" });
+  const [values, handleChange] = useForm({
+    email: "",
+    password: "",
+    firstName: ""
+  });
+
+  useEffect(() => {
+    console.log("render");
+  }, [values.password, values.email]);
 
   return (
     <div className="App">
-      <input name="email" value={values.email} onChange={handleChange} />
+      <input
+        name="email"
+        value={values.email}
+        onChange={handleChange}
+        placeholder="Email"
+      />
+      <input
+        name="firstName"
+        value={values.firstName}
+        onChange={handleChange}
+        placeholder="First Name"
+      />
       <input
         type="password"
         name="password"
         value={values.password}
         onChange={handleChange}
+        placeholder="password"
       />
     </div>
   );
