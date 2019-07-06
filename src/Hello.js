0cec11f3f821181c1b410ee0bba6cba6e358c264
Chau Dinh <chaudvb95uc@gmail.com>
2019-07-06 15:39:47 +0700

    useLayoutEffect - display rectangle

--- src/Hello.js
+++ src/Hello.js
@@ -12,10 +12,11 @@ export const Hello = () => {
     localStorage.setItem("count", JSON.stringify(count));
   }, [count]);
 
+  const [rect, setRect] = useState({});
   const divRef = useRef();
 
   useLayoutEffect(() => {
-    console.log(divRef.current.getBoundingClientRect());
+    setRect(divRef.current.getBoundingClientRect());
   }, [data]);
 
   return (
@@ -23,6 +24,8 @@ export const Hello = () => {
       <div style={{ display: "flex" }}>
         <div ref={divRef}>{loading ? "loading..." : data}</div>
       </div>
+      {/* display the rectangle */}
+      <pre>{JSON.stringify(rect, null, 2)}</pre>
       <div>count: {count}</div>
       <button onClick={() => setCount(c => c + 1)}>increment</button>
     </div>
