22d1d4e1b92fb95ea80a1d596a0d0d857286b926
Chau Dinh <chaudvb95uc@gmail.com>
2019-07-08 13:43:17 +0700

    useCallback hook - useCallback to prevent render when updating the count

--- src/App.js
+++ src/App.js
@@ -1,56 +1,20 @@
-import React, { useState, useRef } from "react";
-import "./App.css";
-import { useForm } from "./useForm";
+import React, { useState, useCallback } from "react";
 import { Hello } from "./Hello";
-import { useMeasure } from "./useMeasure";
 
 const App = () => {
-  const [values, handleChange] = useForm({
-    email: "",
-    password: "",
-    firstName: ""
-  });
-  const inputRef = useRef();
-  const helloLog = useRef(() => console.log("hello"));
+  const [count, setCount] = useState(0);
 
-  const [hello, setHello] = useState(true);
-
-  const [rect, inputRef2] = useMeasure([]);
+  const increment = useCallback(
+    n => {
+      setCount(current => current + n);
+    },
+    [setCount]
+  );
 
   return (
     <div className="App">
-      <button onClick={() => setHello(!hello)}>Toggle</button>
-      {hello && <Hello />}
-      <input
-        ref={inputRef}
-        name="email"
-        value={values.email}
-        onChange={handleChange}
-        placeholder="Email"
-      />
-      <input
-        ref={inputRef2}
-        name="firstName"
-        value={values.firstName}
-        onChange={handleChange}
-        placeholder="First Name"
-      />
-      <input
-        type="password"
-        name="password"
-        value={values.password}
-        onChange={handleChange}
-        placeholder="password"
-      />
-
-      <button
-        onClick={() => {
-          inputRef.current.focus();
-          helloLog.current();
-        }}
-      >
-        focus
-      </button>
+      <Hello increment={increment} />
+      <div>count: {count}</div>
     </div>
   );
 };
--- src/Hello.js
+++ src/Hello.js
@@ -1,27 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { useFetch } from "./useFetch";
-import { useMeasure } from "./useMeasure";
+import React from "react";
+import { useCountRenders } from "./useCountRenders";
 
-export const Hello = () => {
-  const [count, setCount] = useState(
-    () => JSON.parse(localStorage.getItem("count")) || 0
-  );
-  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
-  useEffect(() => {
-    localStorage.setItem("count", JSON.stringify(count));
-  }, [count]);
-
-  const [rect, divRef] = useMeasure([data]);
-
-  return (
-    <div>
-      <div style={{ display: "flex" }}>
-        <div ref={divRef}>{loading ? "loading..." : data}</div>
-      </div>
-      {/* display the rectangle */}
-      <pre>{JSON.stringify(rect, null, 2)}</pre>
-      <div>count: {count}</div>
-      <button onClick={() => setCount(c => c + 1)}>increment</button>
-    </div>
-  );
-};
+export const Hello = React.memo(({ increment }) => {
+  useCountRenders();
+  return <button onClick={() => increment(5)}>Hello</button>;
+});
